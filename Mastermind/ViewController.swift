a39bdb4013d084536fa13fda00f34bed9fb1bfea
FONTAINE Louis <louis.fontaine@efrei.net>
2019-03-13 10:46:57 +0100

    Initial Commit

---
+++ Mastermind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mastermind
+//
+//  Created by Louis Fontaine on 13/03/2019.
+//  Copyright © 2019 Louis Fontaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
